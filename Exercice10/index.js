fafbd5108cb6c0ba5083e0bc4dbb7ca7aba5221e
0X4D7963617374 <olivier@helioscast.com>
2024-05-16 14:48:18 +0200

    exercice 10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,24 @@
-//votre code ici
+function printNumbers(n)
+{
+    let numbers = '';
+    let i;
 
-export default printNumbers
+    for (i = 1; i <= n; i++)
+        numbers += i + ' ';
+
+    return numbers.trim();
+}
+
+function ButtonClick()
+{
+    const numberInput = document.getElementById('numberInput');
+    const result = document.getElementById('result');
+    let n = parseInt(numberInput.value);
+
+    if (!isNaN(n))
+        result.innerText = printNumbers(n);
+    else
+        result.innerText = 'Veuillez entrer un nombre valide.';
+}
+
+document.getElementById('printButton').addEventListener('click', ButtonClick);
