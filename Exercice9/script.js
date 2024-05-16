12a288a15438084bb693a550b2a7fec2836f9357
0X4D7963617374 <olivier@helioscast.com>
2024-05-16 13:34:02 +0200

    exercice 9 pour le fun sans IF

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,13 @@
-// votre code ici
+function checkAge(age)
+{// pour le fun de lexo sans if xD
+    const messages =
+    {
+        mineur: "Vous êtes mineur.",
+        majeur: "Vous êtes majeur.",
+        senior: "Vous êtes senior."
+    };
+
+    return ( (age < 18) && messages.mineur) || ((age <= 65) && messages.majeur) || messages.senior;
+}
 
 export default checkAge;
